8c4b14d16d7514c4511d45d7c03903004eb1f772
Siddhant <siddhant-emb-android@embitel.com>
2021-11-13 22:23:35 +0530

    added parcelize

--- app/src/main/java/com/test/android/siddhant/NYTimesApp.kt
+++ app/src/main/java/com/test/android/siddhant/NYTimesApp.kt
@@ -2,11 +2,10 @@ package com.test.android.siddhant
 
 import android.app.Application
 
-@HiltAndroidApp
 class NYTimesApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        
+
     }
 }
